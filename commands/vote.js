ada4e6a850f4c718b1b9cd9b4698f69c3e1ebefe
Jacqueline South <mail@jqln.me>
2025-04-02 14:38:59 -0500

    Update vote.js

--- commands/vote.js
+++ commands/vote.js
@@ -5,8 +5,7 @@ export default {
     },
     execute (message) {
         message.channel.send('Our voting links can be found here: <a:v_Sparkles:1321700445621719090>\n' +
-                             '- meow\n' +
-                             '- meow\n' +
-                             '- meow');
+                             '- https://minecraftservers.org/vote/670727\n' +
+                             '-https://minecraft-server-list.com/server/509876/vote/');
     }
 }
