e892c7d50ffd037e26674c5916060cf78ff45536
Jacqueline South <mail@jqln.me>
2025-04-22 12:06:32 -0500

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -8,8 +8,8 @@ export default {
     execute (message) {
         const java = new EmbedBuilder()
             .setColor(0x2b2d31)
-            .setTitle('How to Join Everia on **Java Edition** ⋆ ｡˚ ⋆')
-            .setDescription('<:f_everiadot:1322411330774241340> Open the Minecraft launcher and select **Java Edition**.\n' + 
+            .setDescription('## How to Join Everia on **Java Edition** ⋆ ｡˚ ⋆\n' +
+                            '<:f_everiadot:1322411330774241340> Open the Minecraft launcher and select **Java Edition**.\n' + 
                             '<:g_everiadot:1322411242756898826> Set the installation version to `1.21.3` and click **Play**.\n' + 
                             '<:f_everiadot:1322411330774241340> Click **Multiplayer** on the main menu.\n' + 
                             '<:g_everiadot:1322411242756898826> Click **Add Server**.\n' + 
@@ -19,8 +19,8 @@ export default {
 
         const bedrock = new EmbedBuilder()
             .setColor(0x2b2d31)
-            .setTitle('How to Join Everia on **Bedrock Edition** ⋆ ｡˚ ⋆')
-            .setDescription('<:f_everiadot:1322411330774241340> Open the Minecraft app.\n' + 
+            .setDescription('## How to Join Everia on **Bedrock Edition** ⋆ ｡˚ ⋆\n' +
+                            '<:f_everiadot:1322411330774241340> Open the Minecraft app.\n' + 
                             '<:g_everiadot:1322411242756898826> Click **Play** on the main menu.\n' + 
                             '<:f_everiadot:1322411330774241340> Click the **Servers** tab at the top right.\n' + 
                             '<:g_everiadot:1322411242756898826> Scroll to the bottom of the screen and click **Add Server**.\n' + 
@@ -28,7 +28,8 @@ export default {
                             '<:g_everiadot:1322411242756898826> Type `25565` in the **Port** field.\n' + 
                             '<:f_everiadot:1322411330774241340> Click **Play** to join the SMP!\n' +
                             '## Video Tutorials ⋆ ｡˚ ⋆\n' +
-                            '<:f_everiadot:1322411330774241340> [**Bedrock Mobile**](https://www.tiktok.com/t/ZTjjBy1X4/)')
+                            '<:f_everiadot:1322411330774241340> [**Bedrock Mobile**](https://www.tiktok.com/t/ZTjjBy1X4/)\n' +
+                            '<:g_everiadot:1322411242756898826> [**Bedrock Console**](https://www.youtube.com/watch?v=6OsQtv5Nz2Y)')
             .setImage('https://i.imgur.com/8fD0ASX.png');
 
         message.channel.send({ embeds: [java, bedrock] });
